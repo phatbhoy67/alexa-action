da795c25a7c7e0ae4123c8d50e9a385f70d2f8af
phatbhoy67 <chris.scanlon72@gmail.com>
2019-11-19 19:49:23 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -4,27 +4,30 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const baseUrl = "https://www.alexa.com/siteinfo/";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const siteUrl = core.getInput("site-url");
-  console.log(`Hello ${siteUrl}!`);
+async function getAlexaSiteRank() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const siteUrl = core.getInput("site-url");
+    console.log(`Hello ${siteUrl}!`);
 
-  const response = await axios.get(baseUrl + siteUrl);
-  const pageHTML = await response.data;
-  const $ = cheerio.load(pageHTML);
-  let commaCut = /\,/g;
-  let rankMini = $(".rankmini-rank")
-    .html()
-    .trim()
-    .replace("<span>#</span>", "")
-    .replace(commaCut, "");
-  console.log(rankMini);
-  const rankValue = parseInt(rankMini, 10);
-  console.log(typeof rankValue, rankValue);
-  core.setOutput("rank", rankValue);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+    const response = await axios.get(baseUrl + siteUrl);
+    const pageHTML = await response.data;
+    const $ = cheerio.load(pageHTML);
+    let commaCut = /\,/g;
+    let rankMini = $(".rankmini-rank")
+      .html()
+      .trim()
+      .replace("<span>#</span>", "")
+      .replace(commaCut, "");
+    console.log(rankMini);
+    const rankValue = parseInt(rankMini, 10);
+    console.log(typeof rankValue, rankValue);
+    core.setOutput("rank", rankValue);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+getAlexaSiteRank();
